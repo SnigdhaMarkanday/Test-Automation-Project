006537e7f380a9ca2e4a8f273c65fde25ee346dc
SnigdhaMarkanday <59870580+SnigdhaMarkanday@users.noreply.github.com>
2022-04-07 08:20:12 +0530

    Registration Test

---
+++ Experiment-5/RegTest.java
@@ -0,0 +1,49 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class Proj9
+{
+	WebDriver driver;
+	
+	@BeforeTest
+	public void beforeLogin()
+	{
+		System.out.println("Test Automation Project :: Login Test");
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\snigdha\\Desktop\\Chrome Driver\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		
+		driver.get("https://www.primevideo.com/");
+	}
+	
+	@Test
+	public void login() 
+	{
+		driver.findElement(By.id("pv-nav-sign-in")).click();
+		
+		WebElement email = driver.findElement(By.id("ap_email"));
+		email.sendKeys("500077256@stu.upes.ac.in");
+		
+		//WebElement continueButton = driver.findElement(By.id("continue"));
+		//continueButton.click();
+		
+		WebElement password = driver.findElement(By.id("ap_password"));
+		password.sendKeys("R171219035");
+			
+		WebElement loginButton = driver.findElement(By.id("signInSubmit"));
+		loginButton.click();
+  	}
+	
+	@AfterTest
+	public void afterLogin()
+	{
+		driver.close();
+	}
+}
+
